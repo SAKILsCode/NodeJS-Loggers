c7b9f2b448658604b601116238e20dfc20686d20
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2022-07-17 00:28:06 +0600

    Winston - Formatting log

--- src/logger.js
+++ src/logger.js
@@ -10,12 +10,23 @@ const logger = {
 };
 */
 
+const format = winston.format;
+
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.Console()
-  ],
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.align(),
+    format.printf(({ level, message, timestamp, ...args }) => {
+      const ts = timestamp.slice(0, 19).replace('T', ' ');
+      return `${ts} ${level}: ${message} ${
+        Object.keys(args).length ? JSON.stringify(args, null, 2) : ''
+      }`;
+    })
+  ),
+
+  transports: [new winston.transports.Console()],
 });
 
 module.exports = logger;
