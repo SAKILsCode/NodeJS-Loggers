770e4a194dd3ed3a888a15b61f450769042353ba
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2022-07-16 19:49:44 +0600

    Morgan - Formatting log message

--- src/morgan.js
+++ src/morgan.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
+const uuid = require('uuid');
 const app = express();
 
 // app.use(morgan('combined'));
 // app.use(morgan('common'));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 // app.use(morgan('short'));
 // app.use(morgan('tiny'));
+
+app.use((req, _res, next) => {
+  const id = uuid.v4()
+  req.id = id
+  next()
+})
+
+morgan.token('random', (req, res) => {
+  return Math.round(Math.random() * 100);
+});
+
+morgan.token('request-id', (req, res) => {
+  return req.id;
+});
+
+app.use(morgan(':date[iso] | :method | :url | :status | :response-time[4]ms | RandomNum: :random | Request ID: :request-id'));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'welcome' });
