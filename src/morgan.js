1a3a9ae2d19a46510459517e494252ec0d2997da
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2022-07-16 23:11:26 +0600

    Morgan - Prod configuration

--- src/morgan.js
+++ src/morgan.js
@@ -4,11 +4,13 @@ const useMorgan = require('./useMorgan');
 const uuid = require('uuid');
 const app = express();
 
-// app.use(morgan('combined'));
-// app.use(morgan('common'));
-// app.use(morgan('dev'));
-// app.use(morgan('short'));
-// app.use(morgan('tiny'));
+/*
+app.use(morgan('combined'));
+app.use(morgan('common'));
+app.use(morgan('dev'));
+app.use(morgan('short'));
+app.use(morgan('tiny'));
+*/
 
 app.use((req, _res, next) => {
   const id = uuid.v4();
@@ -17,29 +19,10 @@ app.use((req, _res, next) => {
 });
 
 /*
-morgan.token('random', (req, res) => {
-  return Math.round(Math.random() * 100);
-});
-
-morgan.token('request-id', (req, res) => {
-  return req.id;
-});
-
-// app.use(
-//   morgan(
-//     ':date[iso] | :method | :url | :status | :response-time[4]ms | RandomNum: :random | Request ID: :request-id'
-//   )
-// );
-
 app.use(
-  morgan((tokens, req, res) => {
-    return JSON.stringify({
-      method: tokens['method'](req, res),
-      status: tokens['status'](req, res),
-      random: tokens['random'](req, res),
-      requestId: tokens['request-id'](req, res),
-    });
-  })
+  morgan(
+    ':date[iso] | :method | :url | :status | :response-time[4]ms | RandomNum: :random | Request ID: :request-id'
+  )
 );
 */
 
--- src/useMorgan.js
+++ src/useMorgan.js
@@ -1,8 +1,23 @@
 const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+morgan.token('random', (req, res) => {
+  return Math.round(Math.random() * 100);
+});
+
+morgan.token('request-id', (req, res) => {
+  return req.id;
+});
+
+const accessLogStream = fs.createWriteStream(
+  path.resolve('logs', 'access.log'),
+  { flags: 'a' }
+);
 
 const useMorgan = (app) => {
-  if (process.env.NODE_ENV === 'production') {
-    // will update later
+  if (process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'production') {
+    app.use(morgan(prodFormat, { stream: accessLogStream }));
   } else {
     app.use(
       morgan('dev', {
@@ -22,6 +37,15 @@ const useMorgan = (app) => {
       })
     );
   }
+};
+
+const prodFormat = (tokens, req, res) => {
+  return JSON.stringify({
+    method: tokens['method'](req, res),
+    status: tokens['status'](req, res),
+    random: tokens['random'](req, res),
+    requestId: tokens['request-id'](req, res),
+  });
 };
 
 module.exports = useMorgan;
